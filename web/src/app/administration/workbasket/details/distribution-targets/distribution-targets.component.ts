7877b6a82d5e39282e24c35031f89bfa59f9836c
Nacho <JoseIgnacio.RecuerdaCambil@novatec-gmbh.de>
2018-11-06 14:36:55 +0100

    TSK-647 - [UI] Add pagination or infite scrolling to distribution targets lists in order to avoid loading the whole workbasket list.

--- web/src/app/administration/workbasket/details/distribution-targets/distribution-targets.component.ts
+++ web/src/app/administration/workbasket/details/distribution-targets/distribution-targets.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, SimpleChanges, OnChanges } from '@angular/core';
+import { Component, Input, OnDestroy, SimpleChanges, OnChanges, ViewChild, ElementRef } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { Workbasket } from 'app/models/workbasket';
@@ -14,6 +14,10 @@ import { AlertService } from 'app/services/alert/alert.service';
 import { SavingWorkbasketService, SavingInformation } from 'app/administration/services/saving-workbaskets/saving-workbaskets.service';
 import { ErrorModalService } from 'app/services/errorModal/error-modal.service';
 import { RequestInProgressService } from 'app/services/requestInProgress/request-in-progress.service';
+import { TaskanaQueryParameters } from 'app/shared/util/query-parameters';
+import { Page } from 'app/models/page';
+import { OrientationService } from 'app/services/orientation/orientation.service';
+import { Orientation } from 'app/models/orientation';
 
 export enum Side {
 	LEFT,
@@ -37,7 +41,8 @@ export class DistributionTargetsComponent implements OnChanges, OnDestroy {
 	distributionTargetsSubscription: Subscription;
 	workbasketSubscription: Subscription;
 	workbasketFilterSubscription: Subscription;
-	savingDistributionTargetsSubscription: Subscription;
+  savingDistributionTargetsSubscription: Subscription;
+  orientationSubscription: Subscription;
 
 	distributionTargetsSelectedResource: WorkbasketDistributionTargetsResource;
 	distributionTargetsLeft: Array<WorkbasketSummary>;
@@ -47,17 +52,27 @@ export class DistributionTargetsComponent implements OnChanges, OnDestroy {
 	distributionTargetsSelectedClone: Array<WorkbasketSummary>;
 
 	requestInProgressLeft = false;
-	requestInProgressRight = false;
+  requestInProgressRight = false;
+  loadingItems = false
 	modalErrorMessage: string;
 	side = Side;
-	private initialized = false;
+  private initialized = false;
+
+  pageRight: Page;
+  pageLeft: Page;
+  cards: number;
+  heightList: number;
+
+  @ViewChild('panelBody')
+  private panelBody: ElementRef;
 
 	constructor(
 		private workbasketService: WorkbasketService,
 		private alertService: AlertService,
 		private savingWorkbaskets: SavingWorkbasketService,
 		private errorModalService: ErrorModalService,
-		private requestInProgressService: RequestInProgressService) { }
+    private requestInProgressService: RequestInProgressService,
+    private orientationService: OrientationService) { }
 
 	ngOnChanges(changes: SimpleChanges): void {
 		if (!this.initialized && changes.active && changes.active.currentValue === 'distributionTargets') {
@@ -80,16 +95,10 @@ export class DistributionTargetsComponent implements OnChanges, OnDestroy {
 					this.distributionTargetsSelectedResource = distributionTargetsSelectedResource;
 					this.distributionTargetsSelected = distributionTargetsSelectedResource._embedded ?
 						distributionTargetsSelectedResource._embedded.distributionTargets : [];
-					this.distributionTargetsSelectedClone = Object.assign([], this.distributionTargetsSelected);
-					this.workbasketSubscription = this.workbasketService.getWorkBasketsSummary(true,
-						undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined,
-						undefined, undefined, undefined, true).subscribe(
-							(distributionTargetsAvailable: WorkbasketSummaryResource) => {
-								this.distributionTargetsLeft = Object.assign([], distributionTargetsAvailable._embedded.workbaskets);
-								this.distributionTargetsRight = Object.assign([], distributionTargetsAvailable._embedded.workbaskets);
-								this.distributionTargetsClone = Object.assign([], distributionTargetsAvailable._embedded.workbaskets);
-								this.onRequest(undefined, true);
-							});
+          this.distributionTargetsSelectedClone = Object.assign([], this.distributionTargetsSelected);
+          TaskanaQueryParameters.page = 1;
+          this.calculateNumberItemsList();
+          this.getWorkbaskets();
 				});
 
 		this.savingDistributionTargetsSubscription = this.savingWorkbaskets.triggeredDistributionTargetsSaving()
@@ -100,19 +109,82 @@ export class DistributionTargetsComponent implements OnChanges, OnDestroy {
 				}
 			});
 
-	}
+    this.orientationSubscription = this.orientationService.getOrientation().subscribe((orientation: Orientation) => {
+      this.calculateNumberItemsList();
+      this.getWorkbaskets();
+    });
+  }
+
+  private calculateNumberItemsList() {
+    if (this.panelBody) {
+      const cardHeight = 72;
+      this.cards = this.orientationService.calculateNumberItemsList(this.panelBody.nativeElement.offsetHeight, cardHeight, 100, true);
+      this.heightList = this.cards * cardHeight;
+    }
+  }
+
+  onScroll(side: Side) {
+    if (side === this.side.LEFT && this.pageLeft.totalPages > TaskanaQueryParameters.page) {
+      this.loadingItems = true;
+      TaskanaQueryParameters.page = TaskanaQueryParameters.page + 1;
+      this.getWorkbaskets();
+    }
+  }
+
+  private getWorkbaskets(side?: Side) {
+    if (!this.distributionTargetsLeft) {
+      this.distributionTargetsLeft = [];
+    }
+    if (!this.distributionTargetsRight) {
+      this.distributionTargetsRight = [];
+    }
+    if (this.distributionTargetsSelected) {
+      TaskanaQueryParameters.pageSize = this.cards + this.distributionTargetsSelected.length;
+    }
+
+    this.workbasketSubscription = this.workbasketService.getWorkBasketsSummary(true,
+      undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined,
+      undefined, undefined, undefined, false).subscribe(
+        (distributionTargetsAvailable: WorkbasketSummaryResource) => {
+          if (TaskanaQueryParameters.page === 1) {
+            this.distributionTargetsLeft = [];
+          }
+          if (side === this.side.LEFT) {
+            this.distributionTargetsLeft.push(...distributionTargetsAvailable._embedded.workbaskets);
+          } else if (side === this.side.RIGHT) {
+            this.distributionTargetsRight = Object.assign([], distributionTargetsAvailable._embedded.workbaskets);
+          } else {
+            this.distributionTargetsLeft.push(...distributionTargetsAvailable._embedded.workbaskets);
+            this.distributionTargetsRight = Object.assign([], distributionTargetsAvailable._embedded.workbaskets);
+            this.distributionTargetsClone = Object.assign([], distributionTargetsAvailable._embedded.workbaskets);
+          }
+          this.onRequest(undefined, true);
+          if (!this.pageLeft || !this.pageRight) {
+            this.calculatePages(distributionTargetsAvailable);
+          }
+        });
+  }
+
+  private calculatePages(distributionTargetsAvailable) {
+    const distributionTargetsSelected = this.distributionTargetsSelected ? this.distributionTargetsSelected.length : 0;
+    this.pageLeft = new Page(this.cards, (distributionTargetsAvailable.page.totalElements - distributionTargetsSelected),
+      Math.ceil((distributionTargetsAvailable.page.totalElements - distributionTargetsSelected) / this.cards)
+      , distributionTargetsAvailable.page.number);
+    this.pageRight = new Page(this.cards, distributionTargetsSelected,
+      Math.ceil(distributionTargetsSelected / this.cards), TaskanaQueryParameters.page);
+  }
 
 	moveDistributionTargets(side: number) {
 		if (side === Side.LEFT) {
 			const itemsSelected = this.getSelectedItems(this.distributionTargetsLeft, this.distributionTargetsRight)
 			this.distributionTargetsSelected = this.distributionTargetsSelected.concat(itemsSelected);
-			this.distributionTargetsRight = this.distributionTargetsRight.concat(itemsSelected);
+      this.distributionTargetsRight = this.distributionTargetsRight.concat(itemsSelected);
 		} else {
 			const itemsSelected = this.getSelectedItems(this.distributionTargetsRight, this.distributionTargetsLeft);
 			this.distributionTargetsSelected = this.removeSeletedItems(this.distributionTargetsSelected, itemsSelected);
 			this.distributionTargetsRight = this.removeSeletedItems(this.distributionTargetsRight, itemsSelected);
-			this.distributionTargetsLeft = this.distributionTargetsLeft.concat(itemsSelected);
-		}
+      this.distributionTargetsLeft = this.distributionTargetsLeft.concat(itemsSelected);
+    }
 	}
 
 	onSave() {
@@ -178,13 +250,16 @@ export class DistributionTargetsComponent implements OnChanges, OnDestroy {
 	}
 
 	private onRequest(side: Side = undefined, finished: boolean = false) {
+    if (this.loadingItems) {
+      this.loadingItems = false;
+    }
 		if (finished) {
 			side === undefined ? (this.requestInProgressLeft = false, this.requestInProgressRight = false) :
 				side === Side.LEFT ? this.requestInProgressLeft = false : this.requestInProgressRight = false;
 			return;
 		}
 		side === undefined ? (this.requestInProgressLeft = true, this.requestInProgressRight = true) :
-			side === Side.LEFT ? this.requestInProgressLeft = true : this.requestInProgressRight = true;
+      side === Side.LEFT ? this.requestInProgressLeft = true : this.requestInProgressRight = true;
 	}
 
 	private getSeletedIds(): Array<string> {
@@ -199,9 +274,8 @@ export class DistributionTargetsComponent implements OnChanges, OnDestroy {
 		if (this.distributionTargetsSubscription) { this.distributionTargetsSubscription.unsubscribe(); }
 		if (this.workbasketSubscription) { this.workbasketSubscription.unsubscribe(); }
 		if (this.workbasketFilterSubscription) { this.workbasketFilterSubscription.unsubscribe(); }
-		if (this.savingDistributionTargetsSubscription) { this.savingDistributionTargetsSubscription.unsubscribe(); }
-
+    if (this.savingDistributionTargetsSubscription) { this.savingDistributionTargetsSubscription.unsubscribe(); }
+    if (this.orientationSubscription) { this.orientationSubscription.unsubscribe(); }
 	}
 
 }
-
--- web/src/app/administration/workbasket/details/distribution-targets/dual-list/dual-list.component.ts
+++ web/src/app/administration/workbasket/details/distribution-targets/dual-list/dual-list.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChildren } from '@angular/core';
 import { WorkbasketSummary } from 'app/models/workbasket-summary';
 import { FilterModel } from 'app/models/filter';
-import { Side } from '../distribution-targets.component';
+import { Side, DistributionTargetsComponent } from '../distribution-targets.component';
 import { expandDown } from 'app/shared/animations/expand.animation';
 
 @Component({
@@ -13,30 +13,41 @@ import { expandDown } from 'app/shared/animations/expand.animation';
 export class DualListComponent implements OnInit {
 
 	@Input() distributionTargets: Array<WorkbasketSummary>;
-	@Output() distributionTargetsChange = new EventEmitter<Array<WorkbasketSummary>>();
 	@Input() distributionTargetsSelected: Array<WorkbasketSummary>;
 	@Output() performDualListFilter = new EventEmitter<{ filterBy: FilterModel, side: Side }>();
-	@Input() requestInProgress = false;
+  @Input() requestInProgress = false;
+  @Input() loadingItems ? = false;
 	@Input() side: Side;
-	@Input() header: string;
+  @Input() header: string;
+  @Input() heightList: number;
+  @Output() scrolling = new EventEmitter<Side>();
+
+  @ViewChildren(DistributionTargetsComponent) distributionTargetsComponent;
 
 	sideNumber = 0;
 	toggleDtl = false;
-	toolbarState = false;
-
-	constructor() { }
+  toolbarState = false;
 
 	ngOnInit() {
-		this.sideNumber = this.side === Side.LEFT ? 0 : 1;
+    this.sideNumber = this.side === Side.LEFT ? 0 : 1;
 	}
 
 	selectAll(selected: boolean) {
 		this.distributionTargets.forEach((element: any) => {
 			element.selected = selected;
 		});
-	}
+  }
+
+  onScroll() {
+    this.scrolling.emit(this.side);
+  }
 
 	performAvailableFilter(filterModel: FilterModel) {
-		this.performDualListFilter.emit({ filterBy: filterModel, side: this.side });
+    this.performDualListFilter.emit({ filterBy: filterModel, side: this.side });
+  }
+
+  changeToolbarState(state: boolean) {
+    this.toolbarState = state;
+    this.toolbarState ? this.heightList = this.heightList - 95 : this.heightList = this.heightList + 95;
   }
 }
